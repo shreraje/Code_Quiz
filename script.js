dc793b0a5851317653ea5b8f6f1b4410633ee959
Rajendra Shrestha <rajendrashr@gmail.com>
2020-08-25 11:40:53 -0700

    Questions & Option arrays are added

---
+++ script.js
@@ -0,0 +1,53 @@
+
+let questions = [{
+    question: "Where is the correct place to insert a JavaScript?",
+    answer: "Both the <Head> section and the <body> section are correct",
+    option1: "Both the <Head> section and the <body> section are correct",
+    option2: "The <head> section",
+    option3: "The <body> section",
+    option4: "None of above"
+    },{
+    question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    answer: "<script src='xxx.js'>",
+    option1: "<script href='xxx.js'>",
+    option2: "<script src='xxx.js>",
+    option3: "<script name='xxx.js'>",
+    option4: "None of above"
+    }, {
+    question: "How do you create a function in JavaScript?",
+    answer: "function = myFunction()",
+    option1: "function = myFunction()",
+    option2: "function:myFunction()",
+    option3: "function myFunction()",
+    option4: "None of above"
+    },{
+    question: "How to write an IF statement in JavaScript?",
+    answer: "if (i == 5)",
+    option1: "if i = 5",
+    option2: "if (i == 5)",
+    option3: "if i = 5 then",
+    option4: "if i == 5 then"
+    },{
+    question: "How does a FOR loop start?",
+    answer: "for (i = 0; i <= 5; i++)",
+    option1: "for i = 1 to 5",
+    option2: "for (i = 0; i <= 5)",
+    option3: "for (i = 0; i <= 5; i++)",
+    option4: "for (i <= 5; i++)"
+    },{
+    question: "Which event occurs when the user clicks on an HTML element?",
+    answer: "onclick",
+    option1: "onclick",
+    option2: "onmouseclick",
+    option3: "onmouseover",
+    option4: "onchange"
+    },{
+    question: "Which operator is used to assign a value to a variable? (BONUS Question)",
+    answer: "       =",
+    option1: "      *",
+    option2: "      -",
+    option3: "      x",
+    option4: "      ="
+    }];
+
+    
